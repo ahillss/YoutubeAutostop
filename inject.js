aa1eadfd1167ee442bede0e324fae9b8a5eed116
andrewhills <andrewhills@users.noreply.github.com>
2022-01-30 20:39:35 +1100

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,32 +1,32 @@
 var text=`
     void((()=>{
-            var checkExist = setInterval(() => {
-                var c = document.querySelector(".html5-video-container");
-                var aElement = c && c.querySelector('video');
-                var ytplayer = aElement && aElement.parentNode && aElement.parentNode.parentNode;
+        var checkExist = setInterval(() => {
+            var c = document.querySelector(".html5-video-container");
+            var aElement = c && c.querySelector('video');
+            var ytplayer = aElement && aElement.parentNode && aElement.parentNode.parentNode;
+            
+            if(ytplayer) {
+                var vid = ytplayer.getVideoData().video_id;
+                //aElement.simPlay = false; //dunno if this does anything
+                ytplayer.cueVideoById(vid, ytplayer.getCurrentTime());
                 
-                if(ytplayer) {
-                    var vid = ytplayer.getVideoData().video_id;
-                    //aElement.simPlay = false; //dunno if this does anything
-                    ytplayer.cueVideoById(vid, ytplayer.getCurrentTime());
-                    
-                    var seekTo = ytplayer.seekTo;
-                    var playVideo = ytplayer.playVideo;
+                var seekTo = ytplayer.seekTo;
+                var playVideo = ytplayer.playVideo;
 
-                    ytplayer.playVideo = () => {};
-                    ytplayer.seekTo = (tm) => {ytplayer.cueVideoById(vid, tm);};
-                    
-                    ytplayer.addEventListener("onStateChange", function onstate(a) {
-                        if (a != -1) {
-                            ytplayer.playVideo = playVideo;
-                            ytplayer.seekTo = seekTo;
-                            ytplayer.removeEventListener("onStateChange", onstate);
-                        }
-                    });
-                    
-                    clearInterval(checkExist);
-                }
-            }, 333);
+                ytplayer.playVideo = () => {};
+                ytplayer.seekTo = (tm) => {ytplayer.cueVideoById(vid, tm);};
+                
+                ytplayer.addEventListener("onStateChange", function onstate(a) {
+                    if (a != -1) {
+                        ytplayer.playVideo = playVideo;
+                        ytplayer.seekTo = seekTo;
+                        ytplayer.removeEventListener("onStateChange", onstate);
+                    }
+                });
+                
+                clearInterval(checkExist);
+            }
+        }, 333);
     })());
 `;
 
