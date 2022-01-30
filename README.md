695ea8cf0af88e34de2558231c7b64d4a0fab3af
andrewhills <andrewhills@users.noreply.github.com>
2022-01-31 07:02:07 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Youtube Autostop
 
-Chrome extension that loads youtube videos in the unstarted/unloaded state.
-
-Also works with youtube's [pushState-based navigation](https://stackoverflow.com/a/18398921);
-
-Credit: [Stackoverflow](https://stackoverflow.com/a/34100952) and [AutoplayStopper](https://chrome.google.com/webstore/detail/autoplaystopper/ejddcgojdblidajhngkogefpkknnebdh).
+Chrome extension that stops youtube videos auto starting.
--- myscript.js
+++ myscript.js
@@ -1,38 +1,23 @@
-function get_player() {
-    var c = document.querySelector(".html5-video-container");
-    var aElement = c && c.querySelector('video');
-    var ytplayer = aElement && aElement.parentNode && aElement.parentNode.parentNode;
-    return ytplayer;
-}
+var done= false;
 
 function go() {
-    if(!location.pathname.match("(^/watch$)|(^/[uc][^/]*/[^/]+(/featured)?$)")) {
+    if(!location.pathname.match("(^/watch$)|(^/[uc][^/]*/[^/]+(/featured)?$)") || done) {
         return;
     }
     
+    done = true;
+    
     var checkExist = setInterval(() => {
-        var ytplayer = get_player();
-        
-        if(ytplayer) {
-            var vid = ytplayer.getVideoData().video_id;
-            ytplayer.cueVideoById(vid, ytplayer.getCurrentTime());
+        document.querySelectorAll(".html5-video-container").forEach((c)=>{
+            var a = c && c.querySelector('video');
+            var p = a && a.parentNode && a.parentNode.parentNode;
             
-            var seekTo = ytplayer.seekTo;
-            var playVideo = ytplayer.playVideo;
-
-            ytplayer.playVideo = () => {};
-            ytplayer.seekTo = (tm) => {ytplayer.cueVideoById(vid, tm);};
-            
-            ytplayer.addEventListener("onStateChange", function onstate(a) {
-                if (a != -1) {
-                    ytplayer.playVideo = playVideo;
-                    ytplayer.seekTo = seekTo;
-                    ytplayer.removeEventListener("onStateChange", onstate);
-                }
-            });
-            
-            clearInterval(checkExist);
-        }
+            if(p) {
+                p.stopVideo();
+                clearInterval(checkExist);
+                setTimeout(() => {done=false;}, 500);
+            }
+        });
     }, 100);
 }
 
