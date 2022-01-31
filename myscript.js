b1d20d935748255517cde32703733d009c6cf752
andrewhills <andrewhills@users.noreply.github.com>
2022-01-31 15:58:29 +1100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,33 +1,31 @@
-
 function go() {
     if(!location.pathname.match("(^/watch$)|(^/[uc][^/]*/[^/]+(/featured)?$)")) {
         return;
     }
     
     var checkExist = setInterval(() => {
-        let cs = document.querySelectorAll(".html5-video-container");
+        var vs = document.querySelectorAll("video");
         
-        cs.forEach((c)=>{
-            var a = c && c.querySelector('video');
-            var p = a && a.parentNode && a.parentNode.parentNode;
+        if(vs.length!=0) {
+             setTimeout(() => {
+                 clearInterval(checkExist);
+            }, 1500);
+        }
+        
+        vs.forEach((v,i)=>{
+            var p = v.parentNode.parentNode;
             
-            if(p) {
-                clearInterval(checkExist);
-                setTimeout(() => {p.myDone=false;}, 10000);
-                
-                if(p.myDone) {
-                    return;
-                }
-
+            if(!p.myDone) {
                 p.myDone=true;
                 p.cueVideoById(p.getVideoData().video_id,p.getCurrentTime());
-                p.seekTo = () => {};
-                p.playVideo = () => {};
-                //~ p.stopVideo();
+                
+                setTimeout(() => {
+                    p.myDone=false;
+                }, 2000);
             }
         });
     }, 100);
 }
 
-document.addEventListener('yt-navigate-finish', go);
 go();
+document.addEventListener('yt-navigate-finish', go);
